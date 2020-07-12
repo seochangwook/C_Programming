c450a5e68e11ce3b4f9b728bed0dac73b4da073d
seochangwook <scw05313315@gmail.com>
2020-07-12 12:28:59 +0900

    Initial Commit

---
+++ CProject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CProject
+//
+//  Created by seo on 2020/07/12.
+//  Copyright © 2020 seo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
