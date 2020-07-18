f06de0456839eab01ea179b78b61bf566fb23e1b
seochangwook <scw05313315@gmail.com>
2020-07-18 12:15:19 +0900

    printf_scanf

--- CProject/main.c
+++ CProject/main.c
@@ -9,6 +9,32 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    int a;
+    int b;
+    char *name = "seochangwook";
+    
+    a = 10;
+    b = 20;
+    
+    printf("value is a : %d, b : %d\n", a, b);
+    printf("name is : %s\n", name);
+    
+    int input_data;
+    
+    printf("Input Number: ");
+    scanf("%d", &input_data);
+    
+    printf("Input Number is : %d\n", input_data);
+    
+    char input_name[50]; //문자열로 인식
+    
+    printf("Input Name: ");
+    scanf("%s", input_name);
+    
+    int size = strlen(input_name);
+    
+    printf("Input Name is : %s\n", input_name);
+    printf("Size: %d\n", size);
+    
     return 0;
 }
